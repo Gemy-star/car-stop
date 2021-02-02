e501104d29d5a7460981ff7db9a2cc750f612cdc
Mohamed Gamal <Mohamed.Gamal@arabiansystems.com>
2021-02-02 14:44:00 +0200

    structure finilization

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Layout from "./containers/Layout/Layout";
+import { LocalizationProvider } from './context/LocalizationContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <LocalizationProvider>
+              <Layout />
+          </LocalizationProvider>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
