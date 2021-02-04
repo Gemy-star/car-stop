60edd1953574cf03c7c2fbedd7dfa053510929aa
Mohamed Gamal <Mohamed.Gamal@arabiansystems.com>
2021-02-04 15:14:39 +0200

    start_today_container

---
+++ src/containers/StartToday/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import useStyles from './styles';
+import useTheme from '@material-ui/core/styles/useTheme'
+import {useMediaQuery , Typography} from '@material-ui/core';
+
+
+
+const StartToday = props =>   {
+    const classes = useStyles();
+    const theme = useTheme();
+    const isSmallScreen = useMediaQuery(theme.breakpoints.down('xs'));
+    return (
+        <div className={classes.start_today}>
+                <div className={classes.start_div}>
+                    <Typography className={classes.start_today_txt}>
+                        {"Start today!"}
+                    </Typography>
+
+                    <Typography className={classes.you_are}>
+                        {"You are just one click away from selling your car."}
+                    </Typography>
+                </div>
+        </div>
+    )
+}
+export default StartToday;
